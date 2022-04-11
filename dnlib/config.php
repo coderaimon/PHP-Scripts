e8debfd64357493769cf9ea374fbfb609677f352
Aimon Islam <79645318+coderaimon@users.noreply.github.com>
2022-04-12 01:42:33 +0600

    Update config.php

--- dnlib/config.php
+++ dnlib/config.php
@@ -2,9 +2,9 @@
 
 // database connection info
 const DB_HOST = ''; // Database Host
-const DB_USER = 'root'; // Database User
+const DB_USER = ''; // Database User
 const DB_PASS = ''; // Database Password
-const DB_NAME = 'softbyte'; // Database name
+const DB_NAME = ''; // Database name
 
 // Site url 
 const SITE_URL = ''; // Site protocol or url
